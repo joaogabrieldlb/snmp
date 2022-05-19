c361f57b188972b33c3d0b453f6715e84da4d536
Bruno Simm <56409507+BrunoSimm@users.noreply.github.com>
2022-05-18 22:28:50 -0300

    Refactor Gerente

--- src/main/java/com/snmp/gerente/GerenteApp.java
+++ src/main/java/com/snmp/gerente/GerenteApp.java
@@ -1,5 +1,8 @@
 package com.snmp.gerente;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.SocketException;
 import java.security.InvalidParameterException;
 
 import org.snmp4j.CommunityTarget;
@@ -10,10 +13,29 @@ import org.snmp4j.mp.SnmpConstants;
 import org.snmp4j.smi.IpAddress;
 import org.snmp4j.smi.OID;
 import org.snmp4j.smi.OctetString;
+import org.snmp4j.smi.UdpAddress;
 import org.snmp4j.smi.VariableBinding;
 import org.snmp4j.transport.DefaultUdpTransportMapping;
 
 public class GerenteApp {
+
+    private CommunityTarget target;
+    private Snmp snmp;
+    private PDU requestPDU;
+
+    public GerenteApp() throws IOException{
+        // community settings
+        this.target = new CommunityTarget();
+        target.setCommunity(new OctetString("private"));
+        target.setAddress(new UdpAddress(InetAddress.getByName("localhost"), 161));
+        target.setVersion(SnmpConstants.version2c);
+        
+    
+        this.snmp = new Snmp(new DefaultUdpTransportMapping());
+        snmp.listen();
+    
+        this.requestPDU = new PDU();
+    }
     
     public void run(String command, String oid) throws Exception {
         switch (command.toUpperCase()) {
@@ -68,19 +90,23 @@ public class GerenteApp {
     private void executeGetDelta(String oid) {
     }
 
-    private void executeTest(String oid) {
+    private void executeTest(String oid) throws IOException {
         // community settings
         CommunityTarget target = new CommunityTarget();
-        target.setCommunity(new OctetString("public"));
-        target.setAddress(IpAddress.parse("127.0.0.1"));
+        target.setCommunity(new OctetString("private"));
+        target.setAddress(new UdpAddress(InetAddress.getByName("localhost"), 161));
         target.setVersion(SnmpConstants.version2c);
+        
     
         Snmp snmp = new Snmp(new DefaultUdpTransportMapping());
         snmp.listen();
     
         PDU requestPDU = new PDU();
-        requestPDU.add(new VariableBinding(new OID(oid)));
-        ResponseEvent response = snmp.send(requestPDU, target);
+        
+        requestPDU.add(new VariableBinding(new OID(oid), new OctetString("TESTE FOI2")));
+
+        //Quando for SET, precisa setar a comunidade! (private)
+        ResponseEvent response = snmp.set(requestPDU, target);
         // snmp.getNext(pdu, target)
         // snmp.getBulk(pdu, target)
         // snmp.get(pdu, target)
@@ -90,6 +116,7 @@ public class GerenteApp {
         }
         else {
             System.out.println("Received response from: " + response.getPeerAddress());
+            
             // dump response PDU
             System.out.println(response.getResponse().toString());
         }
